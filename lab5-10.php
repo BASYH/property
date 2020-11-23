6493f6caa53f9e277e4e76e5bfc05eea8f599129
BASYH <63577546+BASYH@users.noreply.github.com>
2020-11-23 23:53:29 +0500

    редактирование должника

---
+++ lab5-10.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		<meta charset="UTF-8">
+		<title>ЛР4 Биненда А.</title> 
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="Osn"> 
+		<h4>Биненда А.Д.</h4>
+		<h1>лабораторная работа №4</h1>
+
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "property" ) or die ("Невозможно подключиться к серверу"); // установление соединения с сервером
+//Кодировка данных получаемых из базы
+mysqli_query($connect, "SET NAMES utf8");
+$get_fio = $_GET['fio'];
+	$name = substr($_GET['fio'], 0, (strlen($_GET['fio'])-1));
+	$num = substr($_GET['fio'], -1, 1);
+	$v = "num_res". $num;
+	$hid = "" . $_GET[$v];
+$str = (int)$hid;
+$zapros2="UPDATE debtor SET debt='".$_GET['debt']."', id_man='" .$str ."',  fio_man='" .$name."' where id='".$_GET['id']."'";
+mysqli_query($connect, $zapros2);
+if (mysqli_affected_rows($connect)>0) {
+echo 'Все сохранено. <a href="lr4.php"> <br>Вернуться к списку</a>'; }
+else { echo 'Ошибка сохранения. <a href="lr4.php"><br> Вернуться к списку</a> '; }
+?> 
+		
+</div>
+	</body>
+</html
---
+++ lab5-9.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		<meta charset="UTF-8">
+		<title>ЛР4 Биненда А.</title> 
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="Osn"> 
+		<h4>Биненда А.Д.</h4>
+		<h1>лабораторная работа №4</h1>
+		<H2>Редактирование данных должника:</H2>
+
+<?php
+$connect = mysqli_connect("localhost", "root", "root", "property" ) or die ("Невозможно подключиться к серверу"); // установление соединения с сервером
+//Кодировка данных получаемых из базы
+mysqli_query($connect, "SET NAMES utf8");
+$rows=mysqli_query($connect, "SELECT fio_man, debt FROM debtor WHERE id=".$_GET['id']);
+while ($st = mysqli_fetch_array($rows)) {
+$id=$_GET['id'];
+$fio_man = $st['fio_man'];
+$debt = $st['debt'];
+}
+print "<form action='lab5-10.php' method='get'>";
+$rows1 = mysqli_query($connect, "SELECT id, fio FROM tenants ORDER BY id");
+$i=0;
+        while ($st = mysqli_fetch_array($rows1)) {
+            $fio[$i] = $st['fio'];
+            $id_zh[$i] = $st['id'];
+            $i++;
+        }
+?>
+ФИО:
+<select name='fio'>
+        <?php
+        for($i = 0; $fio[$i] != null; $i++): ?>
+            <option value="<?=$fio[$i].$i?>"><?=$fio[$i]?></option>
+        <?php endfor;
+        for($i = 0; $fio[$i] != null; $i++){
+$name = "num_res". $i;
+$value = "" . $id_zh[$i];
+print "<input type='hidden' name='".$name."' value='".$value."'>";
+}
+        ?>
+        </select><br>
+<?php
+print "<input type='hidden' name='id' value='".$id."'> <br>";
+print "<br>Долг: <input name='debt' size='20' type='varchar'value='".$debt."'><br>";
+print "<input type='submit' name='' value='Сохранить'>";
+print "</form>";
+print "<p><a href=\"lr4.php\"> Вернуться к списку</a>";
+?> 
+		
+</div>
+	</body>
+</html>
