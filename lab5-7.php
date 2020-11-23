32a55932d3c9cfb1ca4499c6835ff28566d964a1
BASYH <63577546+BASYH@users.noreply.github.com>
2020-11-23 23:15:43 +0500

    добавление нового должника

---
+++ lab5-7.php
@@ -0,0 +1,50 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		<meta charset="UTF-8">
+		<title>ЛР4-5 Биненда А.</title> 
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="Osn"> 
+		<h4>Биненда А.Д.</h4>
+		<h1>лабораторная работа №4-5</h1>
+		<H2>Добавление нового должника:</H2>
+<?
+// Подключение к базе данных:
+$connect = mysqli_connect("localhost", "root", "root", "property" ) or die ("Невозможно подключиться к серверу"); // установление соединения с сервером
+//Кодировка данных получаемых из базы
+mysqli_query($connect, "SET NAMES utf8");
+
+$rows = mysqli_query($connect, "SELECT id, fio FROM tenants ORDER BY id");
+$i=0;
+        while ($st = mysqli_fetch_array($rows)) {
+            $fio[$i] = $st['fio'];
+            $id_zh[$i] = $st['id'];
+            $i++;
+        }
+?>
+<form action="lab5-8.php" method="get">
+<br>ФИО: 
+<select name='fio'>
+        <?php
+        for($i = 0; $fio[$i] != null; $i++): ?>
+            <option value="<?=$fio[$i].$i?>"><?=$fio[$i]?></option>
+        <?php endfor;
+        for($i = 0; $fio[$i] != null; $i++){
+$name = "num_res". $i;
+$value = "" . $id_zh[$i];
+print "<input type='hidden' name='".$name."' value='".$value."'>";
+}
+        ?>
+        </select><br>
+Долг: <input name="dolg" size="20" type="varchar">
+</textarea>
+<p><input name="add" type="submit" value="Добавить">
+</form>
+<p>  
+
+<p><a href="lr4.php" class="link">назад</a></p>	
+</div>
+	</body>
+</html>
---
+++ lab5-8.php
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>	
+		<meta charset="UTF-8">
+		<title>ЛР4-5 Биненда А.</title> 
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="Osn"> 
+		<h4>Биненда А.Д.</h4>
+		<h1>лабораторная работа №4-5</h1>
+		<H2>Добавление нового должника:</H2>
+<?php
+// Подключение к базе данных:
+$connect1 = mysqli_connect("localhost", "root", "root", "property" ) or die ("Невозможно подключиться к серверу"); // установление соединения с сервером
+//Кодировка данных получаемых из базы
+mysqli_query($connect1, "SET NAMES utf8");
+$get_fio = $_GET['fio'];
+	$name = substr($_GET['fio'], 0, (strlen($_GET['fio'])-1));
+	$num = substr($_GET['fio'], -1, 1);
+	$v = "num_res". $num;
+	$hid = "" . $_GET[$v];
+$str = (int)$hid;
+
+$sql_add = "INSERT INTO debtor SET debt='".$_GET['dolg']."', id_man='" .$str ."', fio_man='" .$name."'";
+
+mysqli_query($connect1, $sql_add);
+if (mysqli_affected_rows($connect1)>0) // если нет ошибок при выполнении запроса
+{ print "<p>Спасибо, вы зарегистрировали должника в базе данных.";
+print "<p><a href=\"lr4.php\"> Вернуться к списку</a>"; }
+else { print "Ошибка сохранения.".mysqli_error($connect1)." <a href=\"lr4.php\"> Вернуться к списку</a>"; }
+
+?>
+		
+</div>
+	</body>
+</html>
